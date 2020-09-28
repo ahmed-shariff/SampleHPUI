ac93e6ca5e1f277ee6d9e8edfe7eb10b6e297b54
Ahmed Shariff <shariff.mfa@outlook.com>
2020-09-28 04:07:25 -0500

    Code cleanup for core components

---
+++ Assets/HandInterface/scripts/Core/Deformable Display/CalibrateButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HPUI.Core
+{
+    public class CalibrateButton : MonoBehaviour
+    {
+
+	public BtnMapperStatic btnMapperStatic;
+	public HandCoordinateGetter handCoordinateGetter;
+
+	private void OnTriggerEnter(Collider other)
+	{
+	    OnClick();
+	}
+
+	public void OnClick()
+	{
+	    if (!handCoordinateGetter.isCalibrated)
+	    {
+		handCoordinateGetter.Calibrate();
+		btnMapperStatic.generateBtns();
+	    }
+	}
+    }
+}
